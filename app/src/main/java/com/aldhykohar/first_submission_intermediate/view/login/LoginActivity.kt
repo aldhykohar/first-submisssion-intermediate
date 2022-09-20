2ef4933a309263f0c13bc15ec070903bf09b842b
aldhykohar <aldhykohar@stimednp.ac.id>
2022-09-20 22:39:15 +0800

    :zap: init porject and create login activity

---
+++ app/src/main/java/com/aldhykohar/first_submission_intermediate/view/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.aldhykohar.first_submission_intermediate.view.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aldhykohar.first_submission_intermediate.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
