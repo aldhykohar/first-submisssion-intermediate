081d400e2950cd51e9f6c2428f178665eafa2cef
aldhykohar <aldhykohar@stimednp.ac.id>
2022-10-03 21:10:43 +0800

    :zap: create login ui

--- app/src/main/java/com/aldhykohar/first_submission_intermediate/view/login/LoginActivity.kt
+++ app/src/main/java/com/aldhykohar/first_submission_intermediate/view/login/LoginActivity.kt
@@ -3,10 +3,12 @@ package com.aldhykohar.first_submission_intermediate.view.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.aldhykohar.first_submission_intermediate.R
+import com.aldhykohar.first_submission_intermediate.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private val binding: ActivityLoginBinding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
     }
 }
