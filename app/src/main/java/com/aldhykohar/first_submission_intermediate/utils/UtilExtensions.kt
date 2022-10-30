104f745bcf301cbb262575858256348a45e493df
aldhykohar <aldhykohar@stimednp.ac.id>
2022-10-30 14:21:48 +0800

    :zap: finish ui add story

--- app/src/main/java/com/aldhykohar/first_submission_intermediate/utils/UtilExtensions.kt
+++ app/src/main/java/com/aldhykohar/first_submission_intermediate/utils/UtilExtensions.kt
@@ -1,10 +1,18 @@
 package com.aldhykohar.first_submission_intermediate.utils
 
+import android.app.Application
+import android.content.ContentResolver
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import android.graphics.Paint
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.text.Editable
 import android.text.Html
 import android.text.SpannableString
@@ -13,6 +21,13 @@ import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.aldhykohar.first_submission_intermediate.R
+import com.aldhykohar.first_submission_intermediate.utils.UtilConstants.FILENAME_FORMAT
+import com.aldhykohar.first_submission_intermediate.view.add_story.AddStoryActivity
+import java.io.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 object UtilExtensions {
@@ -32,6 +47,10 @@ object UtilExtensions {
 
     fun Context.myError(message: String) {
         Toast.makeText(this, "ERROR : $message", Toast.LENGTH_LONG).show()
+    }
+
+    fun Context.allPermissionsGranted() = UtilConstants.REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
     }
 
     fun EditText.setTextEditable(text: String) {
@@ -64,5 +83,85 @@ object UtilExtensions {
                 return Html.fromHtml(this)
             }
         }
+    }
+
+    private val timeStamp: String = SimpleDateFormat(
+        FILENAME_FORMAT,
+        Locale.US
+    ).format(System.currentTimeMillis())
+
+    fun createTempFile(context: Context): File {
+        val storageDir: File? = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(timeStamp, ".jpg", storageDir)
+    }
+
+    fun createFile(application: Application): File {
+        val mediaDir = application.externalMediaDirs.firstOrNull()?.let {
+            File(it, application.resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
+
+        val outputDirectory = if (
+            mediaDir != null && mediaDir.exists()
+        ) mediaDir else application.filesDir
+
+        return File(outputDirectory, "$timeStamp.jpg")
+    }
+
+    fun rotateBitmap(bitmap: Bitmap, isBackCamera: Boolean = false): Bitmap {
+        val matrix = Matrix()
+        return if (isBackCamera) {
+//        matrix.postRotate(90f)
+            Bitmap.createBitmap(
+                bitmap,
+                0,
+                0,
+                bitmap.width,
+                bitmap.height,
+                matrix,
+                true
+            )
+        } else {
+//        matrix.postRotate(-90f)
+            matrix.postScale(-1f, 1f, bitmap.width / 2f, bitmap.height / 2f)
+            Bitmap.createBitmap(
+                bitmap,
+                0,
+                0,
+                bitmap.width,
+                bitmap.height,
+                matrix,
+                true
+            )
+        }
+    }
+
+    fun uriToFile(selectedImg: Uri, context: Context): File {
+        val contentResolver: ContentResolver = context.contentResolver
+        val myFile = createTempFile(context)
+
+        val inputStream = contentResolver.openInputStream(selectedImg) as InputStream
+        val outputStream: OutputStream = FileOutputStream(myFile)
+        val buf = ByteArray(1024)
+        var len: Int
+        while (inputStream.read(buf).also { len = it } > 0) outputStream.write(buf, 0, len)
+        outputStream.close()
+        inputStream.close()
+
+        return myFile
+    }
+
+    fun reduceFileImage(file: File): File {
+        val bitmap = BitmapFactory.decodeFile(file.path)
+        var compressQuality = 100
+        var streamLength: Int
+        do {
+            val bmpStream = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.JPEG, compressQuality, bmpStream)
+            val bmpPicByteArray = bmpStream.toByteArray()
+            streamLength = bmpPicByteArray.size
+            compressQuality -= 5
+        } while (streamLength > 1000000)
+        bitmap.compress(Bitmap.CompressFormat.JPEG, compressQuality, FileOutputStream(file))
+        return file
     }
 }
